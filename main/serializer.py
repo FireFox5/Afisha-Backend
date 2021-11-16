f99e6c0cc78f6df9d2901450c343b7dd2360b04b
Sultanmurat <sultanmuratnaspekov@gmail.com>
2021-11-16 23:52:05 +0600

    re-homework

--- main/serializer.py
+++ main/serializer.py
@@ -1,18 +1,28 @@
 from rest_framework import serializers
 from .models import *
+
+
+class CinemaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cinema
+        fields = '__all__'
+
+
+class GenresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genres
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    cinema = CinemaSerializer()
+    genres = GenresSerializer(many=True)
     class Meta:
         model = Movie
         fields = '__all__'
-class CinemaSerializer(serializers.ModelSerializer):
-            class Meta:
-             model = Cinema
-             fields = '__all__'
-class GenresSerializer(serializers.ModelSerializer):
-            class Meta:
-             model = Genres
-             fields = '__all__'
-class ReviewSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Review
-            fields = '__all__'
